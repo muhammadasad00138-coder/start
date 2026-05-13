58807becadf5d8e4cca2dd1eaf69dc13df645b48
muhammadasad00138-coder <muhammadasad00138@gmail.com>
2026-05-13 12:21:09 -0700

    1st push

---
+++ main.cpp
@@ -0,0 +1,36 @@
+ #include <iostream>
+  using namespace std;
+  int main() {
+	  string name = "Muhammad Asad Mukhtar";
+	  string country = "Pakistan";
+	  string city = "Sahiwal";
+	  string cnic = "36502----";
+	  string uni = "Uet top campus";
+	  string dep = "Computer wali science";
+	  string regNo = "2025-cs-583";
+	  string favSong = "arz ka hy";
+	  string favMovie = "Saiyaara";
+	  string countryUlike = "Russia";
+	  string firend = "None";
+	  string hubby = "Gaming , Coding";
+	  string game = "freefire";
+	  string dream = "Become a Successful person inshallah";
+
+	  cout << "Name:" << name << endl;
+	  cout << "Country:" << country << endl;
+	  cout << "City:" << city << endl;
+	  cout << "Cnic no:" << cnic << endl;
+	  cout << "Universty:" << uni << endl;
+	  cout << "Department:" << dep << endl;
+	  cout << "Registration No:" << regNo << endl;
+	  cout << "Fav Song:" << favSong << endl;
+	  cout << "Fav Movie:" << favMovie << endl;
+	  cout << "Country u like:" << countryUlike << endl;
+	  cout << "Any Friend:" << firend << endl;
+	  cout << "Hubby u like:" << hubby << endl;
+	  cout << "Any Game which u like:" << game << endl;
+	  cout << "Dream:" << dream << endl;
+
+	  return 0;
+  }
+  
